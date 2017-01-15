f79a62e833dda790d17781f3c2651352b4416e15
przemek0924 <pm86@tlen.pl>
2017-01-15 12:42:04 +0100

    wszystko

--- malybar/pizza/views.py
+++ malybar/pizza/views.py
@@ -5,6 +5,7 @@ from django.utils.decorators import method_decorator
 from . import models
 from . import forms
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -15,10 +16,12 @@ def index(request):
     kontekst = {'komunikat': 'Witaj w aplikacji Pizza!'}
     return render(request, 'pizza/index.html', kontekst)
 
-def kontakt (request):
+
+def kontakt(request):
     """kontakt"""
     kontekst = {'kontakt': 'budynek A0'}
     return render(request, 'pizza/kontakt.html', kontekst)
+
 
 @method_decorator(login_required, 'dispatch')
 class PizzaCreate(CreateView):
@@ -57,3 +60,56 @@ class PizzaCreate(CreateView):
         return self.render_to_response(
             self.get_context_data(form=form, skladniki=skladniki)
         )
+
+
+@method_decorator(login_required, 'dispatch')
+class PizzaUpdate(UpdateView):
+    """Widok aktualizuacji"""
+
+    model = models.Pizza
+    form_class = forms.PizzaForm
+    success_url = reverse_lazy('pizza:lista')  # '/pizza/lista'
+
+    def get_context_data(self, **kwargs):
+        context = super(PizzaUpdate, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['skladniki'] = forms.SkladnikiFormSet(
+                self.request.POST,
+                instance=self.object)
+        else:
+            context['skladniki'] = forms.SkladnikiFormSet(instance=self.object)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        skladniki = forms.SkladnikiFormSet(
+            self.request.POST,
+            instance=self.object)
+        if form.is_valid() and skladniki.is_valid():
+            return self.form_valid(form, skladniki)
+        else:
+            return self.form_invalid(form, skladniki)
+
+    def form_valid(self, form, skladniki):
+        form.instance.autor = self.request.user
+        self.object = form.save()
+        skladniki.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form, skladniki):
+        return self.render_to_response(
+            self.get_context_data(form=form, skladniki=skladniki)
+        )
+
+
+@method_decorator(login_required, 'dispatch')
+class PizzaDelete(DeleteView):
+    model = models.Pizza
+    success_url = reverse_lazy('pizza:lista')  # '/pizza/lista'
+
+    def get_context_data(self, **kwargs):
+        context = super(PizzaDelete, self).get_context_data(**kwargs)
+        skladniki = models.Skladnik.objects.filter(pizza=self.object)
+        context['skladniki'] = skladniki
+        return context
